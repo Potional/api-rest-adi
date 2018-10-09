6eb894c48bf7d7f3a601fb63d97d1697602d5ab2
Jorge Martin Abad <jomaab71@gmail.com>
2018-10-09 13:28:48 +0200

    a

---
+++ db.sql
@@ -0,0 +1,7 @@
+DROP SCHEMA IF EXISTS `basedatosadi`;
+CREATE DATABASE `basedatosadi`;
+CREATE TABLE `basedatosadi`.`pruba` (
+  `idpruba` INT NOT NULL,
+  `prubacol` VARCHAR(45) NULL,
+  `prubacol1` VARCHAR(45) NULL,
+  PRIMARY KEY (`idpruba`));
