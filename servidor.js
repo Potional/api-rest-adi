31451c86fab4a236c769f28ab718f6e0402d5677
modeloeps <alu@clL04A-6>
2018-10-10 16:53:30 +0200

    mierda

--- servidor.js
+++ servidor.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser);
+//app.use(bodyParser);
 
 //###########Conexion a la base de datos###################################
 
@@ -16,6 +16,28 @@ var knex = require('knex')({
 });
 
 //###############################################################################
+
+//##################################Routas#######################################
+
+app.route('/juegos')
+.get(function(req, res) {
+  console.log('Lista de juegos');
+    listarJuegos(function(datos){
+        resp.send(datos)
+    })
+})
+.post(function(req, res) {
+    console.log('Añadir un juego');
+});
+//################################################################################
+//##################################Acceso a datos################################
+function listarUsuarios(callback) {
+    knex.select().from('Juego')
+    .then(function(datos){
+      callback(datos)
+    })
+}
+//################################################################################
 
 app.listen(3000,function(){
     console.log("¿Esta corriendo el servidor en el puerto 3000? Pa k kiere saber eso, jaja salu2");
