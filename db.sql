0ec3f452ed9c0adc305bfeaadbdc07d1f775fccb
Jorge Martin Abad <jomaab71@gmail.com>
2018-11-06 09:28:53 +0100

    Borrado esto

--- db.sql
+++
@@ -1,7 +0,0 @@
-DROP SCHEMA IF EXISTS `basedatosadi`;
-CREATE DATABASE `basedatosadi`;
-CREATE TABLE `basedatosadi`.`pruba` (
-  `idpruba` INT NOT NULL,
-  `prubacol` VARCHAR(45) NULL,
-  `prubacol1` VARCHAR(45) NULL,
-  PRIMARY KEY (`idpruba`));
